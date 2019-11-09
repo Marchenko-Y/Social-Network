6c7e71a121fddec0a8eb5d58f0013815e2fc6b20
marchenko ulya <marchenko.ulya26@gmail.com>
2019-11-09 15:17:44 +0300

    follow-unfollow api(62)

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -17,7 +17,10 @@ class UsersContainer extends React.Component {
     this.props.toogleIsFenching(true);
     axios
       .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
+        {
+          withCredentials: true
+        }
       )
       .then(response => {
         this.props.toogleIsFenching(false);
@@ -31,7 +34,10 @@ class UsersContainer extends React.Component {
     this.props.toogleIsFenching(true);
     axios
       .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,
+        {
+          withCredentials: true
+        }
       )
       .then(response => {
         this.props.toogleIsFenching(false);
